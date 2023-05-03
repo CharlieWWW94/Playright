ce6b23677affc417f3bd2a8650b5566dc6a6c8de
Charlie Whiteside <charlie.whiteside@rocksteadymusicschool.com>
2023-05-03 11:16:02 +0100

    Create versioned namespace for api with session and registrations controllers

---
+++ app/controllers/api/v1/users/sessions_controller.rb
@@ -0,0 +1,23 @@
+class Api::V1::Users::SessionsController < ApplicationController
+  # Not sure I need this.. when up and running remove.
+  respond_to :json
+
+  private
+
+  def respond_with(resource, _opts={})
+    render json: {message: 'You are now logged in.'}, status: :ok
+  end
+
+  def respond_to_on_destroy
+    log_out_success && return if current_user
+    log_out_failure
+  end
+
+  def log_out_success
+    render json: {message: 'You are now logged out.'}, status: :ok
+  end
+
+  def log_out_failure
+    render json: {message: 'That didn`t seem to work.'}, status: :unauthorized
+  end
+end
