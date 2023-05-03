a7c78c878b803e7ba721c683228fd0b2325c9211
Charlie Whiteside <charlie.whiteside@rocksteadymusicschool.com>
2023-05-03 12:38:25 +0100

    Update routes, schemas etc for authentication/login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users,
+    path: '/api/v1/users',
+    controllers: {
+      sessions: 'api/v1/users/sessions',
+      registrations: 'api/v1/users/registrations'
+    }
+    
+    # Defines the root path route ("/")
+    # root "articles#index"
+    namespace :api, defaults: {format: 'json'} do
+      namespace :v1, defaults: {format: 'json'} do
+        get '/member-data', to: 'users/members#show'
+    end
+  end
 end
